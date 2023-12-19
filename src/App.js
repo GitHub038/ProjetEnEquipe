63f6532d69f529d1e88c9c20aaadba10d9064c06
DayOneAI <bambathiam88@gmail.com>
2023-12-20 00:58:42 +0100

    Setup_supabase_client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import "./App.css";
-
 // TODO : Ici mettre les routes de notre projet
+
+import Authentification from './pages/Authentification'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Futur site de notre projet</p>
+        {/* <Authentification /> */}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Authentification.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Auth from '../components/authentification/Auth'
+
+const Authentification = () => {
+  return (
+    <>
+      <div>Authentification</div>
+      {/* <Auth /> */}
+    </>
+  )
+}
+
+export default Authentification
